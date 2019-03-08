535f31510b32a16b9d4bc2cd9172dd4cfebc787f
Dai Fangyue <dfyshisb@163.com>
2019-03-08 11:37:27 +0800

    Finish Test file

--- src/test/java/WordLadderTest.java
+++ src/test/java/WordLadderTest.java
@@ -1,13 +1,11 @@
-import java.util.Set;
-import java.util.TreeSet;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordLadderTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void generate_string() {
-        Set<String> s = new TreeSet<String>();
         assertEquals("code->cade->cate->date->data", WordLadder.generate_string("code", "data"));
     }
 }
